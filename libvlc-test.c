dd36b9e743eb50940ec953a194eb8d6f1d8fd38f
Christopher Obbard <chris.obbard@collabora.com>
2021-11-15 13:48:34 +0000

    Customise example to better suit developer needs
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- libvlc-test.c
+++ libvlc-test.c
@@ -14,44 +14,43 @@
 #include <SDL2/SDL_mutex.h>
  
 #include <vlc/vlc.h>
- 
-#define WIDTH 640
-#define HEIGHT 480
 
-#define VIDEOWIDTH 320
-#define VIDEOHEIGHT 240
+#define WIDTH 1920
+#define HEIGHT 1080
+
+#define VIDEOWIDTH 1920
+#define VIDEOHEIGHT 1080
 
 typedef struct context {
+    SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
     SDL_mutex *mutex;
-    int n;
 } context;
 
 // VLC prepares to render a video frame.
-static void *lock(void *data, void **p_pixels) {
-
-    struct context *c = (context *)data;
-
+static void *lock(void *data, void **p_pixels)
+{
+    struct context *c = (context *) data;
     int pitch;
+
     SDL_LockMutex(c->mutex);
     SDL_LockTexture(c->texture, NULL, p_pixels, &pitch);
 
-    return NULL; // Picture identifier, not needed here.
+    return NULL;
 }
 
 // VLC just rendered a video frame.
-static void unlock(void *data, void *id, void *const *p_pixels) {
-
-    struct context *c = (context *)data;
-
-    uint16_t *pixels = (uint16_t *)*p_pixels;
+static void unlock(void *data, void *id, void *const *p_pixels)
+{
+    struct context *c = (context *) data;
+    uint16_t *pixels = (uint16_t *) *p_pixels;
+    int x, y;
 
     // We can also render stuff.
-    int x, y;
-    for(y = 10; y < 40; y++) {
-        for(x = 10; x < 40; x++) {
-            if(x < 13 || y < 13 || x > 36 || y > 36) {
+    for (y = 10; y < 40; y++) {
+        for (x = 10; x < 40; x++) {
+            if (x < 13 || y < 13 || x > 36 || y > 36) {
                 pixels[y * VIDEOWIDTH + x] = 0xffff;
             } else {
                 // RV16 = 5+6+5 pixels per color, BGR.
@@ -64,16 +63,18 @@ static void unlock(void *data, void *id, void *const *p_pixels) {
     SDL_UnlockMutex(c->mutex);
 }
 
-// VLC wants to display a video frame.
-static void display(void *data, void *id) {
-
+static void display(void *data, void *id)
+{
     struct context *c = (context *)data;
-
     SDL_Rect rect;
-    rect.w = VIDEOWIDTH;
-    rect.h = VIDEOHEIGHT;
-    rect.x = (int)((1. + .5 * sin(0.03 * c->n)) * (WIDTH - VIDEOWIDTH) / 2);
-    rect.y = (int)((1. + .5 * cos(0.03 * c->n)) * (HEIGHT - VIDEOHEIGHT) / 2);
+    int width, height;
+
+    SDL_GetWindowSize(c->window, &width, &height);
+
+    rect.w = width;
+    rect.h = height;
+    rect.x = 0;
+    rect.y = 0;
 
     SDL_SetRenderDrawColor(c->renderer, 0, 80, 0, 255);
     SDL_RenderClear(c->renderer);
@@ -81,63 +82,56 @@ static void display(void *data, void *id) {
     SDL_RenderPresent(c->renderer);
 }
 
-static void quit(int c) {
+static void quit(int c)
+{
     SDL_Quit();
     exit(c);
 }
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     libvlc_instance_t *libvlc;
     libvlc_media_t *m;
     libvlc_media_player_t *mp;
+    SDL_Event event;
+    bool done = false;
+    struct context context;
+
     char const *vlc_argv[] = {
-
-        "--no-audio", // Don't play audio.
+        "--verbose",
         "--no-xlib", // Don't use Xlib.
-
-        // Apply a video filter.
-        //"--video-filter", "sepia",
-        //"--sepia-intensity=200"
     };
     int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
 
-    SDL_Event event;
-    int done = 0, action = 0, pause = 0;
-
-    struct context context;
-
-    if(argc < 2) {
+    if (argc < 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     // Initialise libSDL.
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Could not initialize SDL: %s.\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
     // Create SDL graphics objects.
-    SDL_Window * window = SDL_CreateWindow(
-            "Fartplayer",
+    context.window = SDL_CreateWindow("libvlc-test",
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
             WIDTH, HEIGHT,
             SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
-    if (!window) {
+    if (!context.window) {
         fprintf(stderr, "Couldn't create window: %s\n", SDL_GetError());
         quit(3);
     }
 
-    context.renderer = SDL_CreateRenderer(window, -1, 0);
+    context.renderer = SDL_CreateRenderer(context.window, -1, 0);
     if (!context.renderer) {
         fprintf(stderr, "Couldn't create renderer: %s\n", SDL_GetError());
         quit(4);
     }
 
-    context.texture = SDL_CreateTexture(
-            context.renderer,
+    context.texture = SDL_CreateTexture(context.renderer,
             SDL_PIXELFORMAT_BGR565, SDL_TEXTUREACCESS_STREAMING,
             VIDEOWIDTH, VIDEOHEIGHT);
     if (!context.texture) {
@@ -168,34 +162,17 @@ int main(int argc, char *argv[]) {
 
     // Main loop.
     while(!done) {
-
-        action = 0;
-
-        // Keys: enter (fullscreen), space (pause), escape (quit).
-        while( SDL_PollEvent( &event )) {
-
-            switch(event.type) {
+        // on any keypress just quit
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
                 case SDL_QUIT:
                     done = 1;
                     break;
                 case SDL_KEYDOWN:
-                    action = event.key.keysym.sym;
+                    done = 1;
                     break;
             }
         }
-
-        switch(action) {
-            case SDLK_ESCAPE:
-            case SDLK_q:
-                done = 1;
-                break;
-            case ' ':
-                printf("Pause toggle.\n");
-                pause = !pause;
-                break;
-        }
-
-        if(!pause) { context.n++; }
 
         SDL_Delay(1000/10);
     }
